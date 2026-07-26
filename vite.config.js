858c58dfca7a70f0c970c0ae5a7e1d6be08ac78b
Atsa <abatsaeth@gmail.com>
2026-07-26 22:16:07 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   server: { host: '0.0.0.0', port: 3000, allowedHosts: 'all' },
   build: {
+    modulePreload: { polyfill: false },
     target: 'esnext',
     minify: 'terser',
-    cssMinify: true,
+    cssMinify: 'lightningcss',
     terserOptions: {
       compress: {
         drop_console: true,
